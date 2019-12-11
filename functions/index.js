fa3c3ad3e792d25ee8dc032e848f24c737322888
espressonist <sopkope@gmail.com>
2019-12-11 16:43:03 +0800

    created cloud function to automatically convert xhtml to json

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+exports.xhtmltojson = functions.storage.object().onFinalize(async (object) => {
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
+    const fileName = path.basename(filePath);
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+        contentType: contentType,
+    };
+
+    await bucket.file(filePath).download({
+        destination: tempFilePath
+    });
+    console.log('File downloaded locally to', tempFilePath);
+
+    return fs.readFile(tempFilePath, function (err, data) {
+        var parser = new xml2js.Parser({
+            explicitArray: false
+        });
+
+        return parser.parseString(data, function (err, xml) {
+            fs.writeFile(tempFilePath, JSON.stringify(xml), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                const jsonName = `json_${fileName}`;
+                const jsonPath = path.join(path.dirname(filePath), jsonName);
+
+                // Uploading the thumbnail.
+                bucket.upload(tempFilePath, {
+                    destination: jsonPath,
+                    metadata: metadata,
+                });
+
+                // Once the thumbnail has been uploaded delete the local file to free up disk space.
+                return fs.unlinkSync(tempFilePath);
+            });
+        });
+    });
+});
