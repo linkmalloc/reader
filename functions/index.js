081f9e39bd3d677d078f7d8efe0afb39e9fe69b8
espressonist <sopkope@gmail.com>
2019-12-13 17:36:21 +0800

    store converted json to Firestore

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,16 @@ const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
+const firestore = require("@google-cloud/firestore");
 admin.initializeApp();
 
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
 const xml2js = require("xml2js");
-const { parseEpub } = require("@gxl/epub-parser");
+const {
+    parseEpub
+} = require("@gxl/epub-parser");
 
 exports.xhtmltojson = functions.storage.object().onFinalize(async object => {
     const fileBucket = object.bucket;
@@ -18,60 +21,47 @@ exports.xhtmltojson = functions.storage.object().onFinalize(async object => {
     const fileName = path.basename(filePath);
     const bucket = admin.storage().bucket(fileBucket);
     const tempFilePath = path.join(os.tmpdir(), fileName);
-    const metadata = {
-        contentType: contentType,
-    };
+    let booksCollection = admin.firestore().collection("books");
 
     // Check if the uploaded file is an epub, if yes, continue, otherwise, exit.
-    if (contentType.startsWith("application/epub")) {
-        // download the epub file to Cloud Functions' temp directory
-        await bucket.file(filePath).download({
-            destination: tempFilePath,
-        });
-        console.log("File downloaded locally to", tempFilePath);
-
-        // Start parsing the epub
-        return parseEpub(tempFilePath).then(result => {
-            let counter = 0;
-
-            // loop each section and convert to json
-            result.sections.forEach(section => {
-                xml2js.parseString(section["htmlString"], function(
-                    err,
-                    result
-                ) {
-                    let jsonName = "converted_" + counter + ".json";
-                    let convertedFile = path.join(os.tmpdir(), jsonName);
-
-                    // create file (the converted json) in the temp directory
-                    fs.writeFile(
-                        convertedFile,
-                        JSON.stringify(result),
-                        function(err) {
-                            if (err) console.log("Error creating json");
-                        }
-                    );
-
-                    // upload the file to Firebase Storage
-                    bucket.upload(convertedFile, {
-                        metadata: { contentType: "application/json" },
-                    });
-                    counter++;
-                });
-            });
-
-            // If done converting and saving, remove all files from temp directory
-            if (counter === result.sections.length) {
-                fs.readdir(os.tmpdir(), (err, files) => {
-                    if (err) throw err;
-
-                    for (const file of files) {
-                        fs.unlinkSync(path.join(os.tmpdir(), file), err => {
-                            if (err) throw err;
-                        });
-                    }
-                });
-            }
-        });
+    if (!contentType.startsWith("application/epub")) {
+        return console.log("Not an epub file.");
     }
+
+    // download the epub file to Cloud Functions' temp directory
+    await bucket.file(filePath).download({
+        destination: tempFilePath,
+    });
+    console.log("File downloaded locally to", tempFilePath);
+
+    // Start parsing the epub
+    parseEpub(tempFilePath).then(result => {
+        let bookInfo = {
+            title: result.info.title,
+            author: result.info.author,
+            publisher: result.info.publisher
+        }
+        let counter = 0;
+        let bookDoc = booksCollection.add(bookInfo);
+        let bookDocRef = booksCollection.doc(bookDoc);
+        let bookSubCollection = bookDocRef.collection("sections");
+
+        // loop each section and convert to json
+        result.sections.forEach(section => {
+            xml2js.parseString(section["htmlString"], function (
+                err,
+                json
+            ) {
+                bookSubCollection.add(json)
+                counter++;
+            });
+        });
+
+        // If done converting and saving, remove file from temp directory
+        if (counter === result.sections.length) {
+            fs.unlinkSync(tempFilePath, err => {
+                if (err) throw err;
+            });
+        }
+    });
 });
